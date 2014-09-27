54bd29b8765b9ce64142f4804f303ec280b1c5fd
Niraj Pant <nirajpant7@gmail.com>
2014-09-27 00:38:49 -0500

    add posting. possible bugs

--- www/js/app.js
+++ www/js/app.js
@@ -1,14 +1,16 @@
 angular.module("FreshlyPressed", ["ionic"])
 .controller("MainCtrl", function($scope, $log, latestStuff) {
+	$scope.posts = [];
 	$scope.refresh = function() {
-		latestStuff.getBlogs();
+		latestStuff.getBlogs($scope);
 	};
 })
 
 .service("latestStuff", function($http, $log) {
-	this.getBlogs = function() {
+	this.getBlogs = function($scope) {
 		$http.jsonp("https://public-api.wordpress.com/rest/v1/freshly-pressed?callback=JSON_CALLBACK")
 			.success(function(result) {
+				$scope.posts = result.posts;
 				$log.info(JSON.stringify(result.posts));
 			});
 	};
